2ec18fa761ff50965194464244ea16cb3bd38fe5
Brian Lee <brnjlee@gmail.com>
2019-09-17 18:23:48 -0400

    Updates

---
+++ precache-manifest.77d1092896c2330caf153818788dbf57.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "903075ada3a9f38883013c72f2d583e8",
+    "url": "/static/media/resume.903075ad.pdf"
+  },
+  {
+    "revision": "d909150d7584bb543def43292ba3d740",
+    "url": "/static/media/q4.d909150d.svg"
+  },
+  {
+    "revision": "e4c3089179376bbca8d247d8eb6a08c5",
+    "url": "/static/media/mockup1.e4c30891.png"
+  },
+  {
+    "revision": "528e317e2f1fddeb18a95839297b21a3",
+    "url": "/static/media/Empire.528e317e.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "717d40a6eddafecdf681",
+    "url": "/static/js/main.717d40a6.chunk.js"
+  },
+  {
+    "revision": "5e2dafc13d5f60d9770c",
+    "url": "/static/js/1.5e2dafc1.chunk.js"
+  },
+  {
+    "revision": "717d40a6eddafecdf681",
+    "url": "/static/css/main.e65e2f0b.chunk.css"
+  },
+  {
+    "revision": "a24e03d2ff0fdc43a061e74d2076c7fc",
+    "url": "/index.html"
+  }
+];
