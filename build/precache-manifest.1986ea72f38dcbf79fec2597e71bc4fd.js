d0c55c43eaae93cb993912d4ce0289d30ada2a99
Brian Lee <brnjlee@gmail.com>
2019-09-17 18:11:13 -0400

    Remove font

---
+++ build/precache-manifest.1986ea72f38dcbf79fec2597e71bc4fd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "903075ada3a9f38883013c72f2d583e8",
+    "url": "/Portfolio-v2/static/media/resume.903075ad.pdf"
+  },
+  {
+    "revision": "d909150d7584bb543def43292ba3d740",
+    "url": "/Portfolio-v2/static/media/q4.d909150d.svg"
+  },
+  {
+    "revision": "e4c3089179376bbca8d247d8eb6a08c5",
+    "url": "/Portfolio-v2/static/media/mockup1.e4c30891.png"
+  },
+  {
+    "revision": "528e317e2f1fddeb18a95839297b21a3",
+    "url": "/Portfolio-v2/static/media/Empire.528e317e.png"
+  },
+  {
+    "revision": "d93163354fb00a1f5486",
+    "url": "/Portfolio-v2/static/js/runtime~main.d9316335.js"
+  },
+  {
+    "revision": "767c8b7811a64f2a2388",
+    "url": "/Portfolio-v2/static/js/main.767c8b78.chunk.js"
+  },
+  {
+    "revision": "38e28b23d5c83d5713b5",
+    "url": "/Portfolio-v2/static/js/1.38e28b23.chunk.js"
+  },
+  {
+    "revision": "767c8b7811a64f2a2388",
+    "url": "/Portfolio-v2/static/css/main.e65e2f0b.chunk.css"
+  },
+  {
+    "revision": "547c92b547f5d11c8ca6f0fadfbb07e7",
+    "url": "/Portfolio-v2/index.html"
+  }
+];
