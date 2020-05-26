9d1989f8123455071481f6d4b2dee09e83f997a9
Brian Lee <brnjlee@gmail.com>
2020-05-26 16:07:41 -0400

    Updates

---
+++ precache-manifest.12075dccd94f4baffa8ecf4a81e830df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c02e45937b6ec81905485aabb34e4cc",
+    "url": "/static/media/resume.7c02e459.pdf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "816ca170dd47f0608206",
+    "url": "/static/js/main.816ca170.chunk.js"
+  },
+  {
+    "revision": "80a9975e9fd5177ba65a",
+    "url": "/static/js/1.80a9975e.chunk.js"
+  },
+  {
+    "revision": "816ca170dd47f0608206",
+    "url": "/static/css/main.ded50fc9.chunk.css"
+  },
+  {
+    "revision": "d7b4fcc699f8315bacc3bfa7636be2de",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.175dd967300bdb3dd756a8b536602315.js"
+  "/precache-manifest.12075dccd94f4baffa8ecf4a81e830df.js"
 );
 
 workbox.clientsClaim();
