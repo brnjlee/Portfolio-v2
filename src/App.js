7a51f160cedf53486a873f6dddf373098b032cfb
Brian Lee <brnjlee@gmail.com>
2020-05-26 12:49:47 -0400

    Update resume

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React, { Component } from 'react';
-import './App.css';
-import './mouse.css';
-import { TimelineMax, TweenLite, CSSPlugin } from "gsap/all";
+import React, { Component } from "react";
+import "./App.css";
+import "./mouse.css";
 import { FiGithub, FiLinkedin, FiFileText } from "react-icons/fi";
-import resume from './assets/resume.pdf';
+import resume from "./assets/resume.pdf";
 
-import Scene from './components/Scene';
+import Scene from "./components/Scene";
 
 class App extends Component {
   constructor() {
@@ -13,93 +12,8 @@ class App extends Component {
     this.state = {
       page: 1,
       pageTo: 0,
-      showCard: null,
+      showCard: null
     };
-    this.data = [
-        {
-          src: require("./assets/Empire.png"),
-          srcWidth: 100,
-          srcHeight: 100,
-          title: "Empire Life, Innovation Lab",
-          description: "Software Developer",
-          body: `
-          • Worked as a core member of the innovation team, producing research and development regarding emerging technologies <br />
-          • Designed and developed a chatbot module integrated with a Support knowledge base <br />
-          • Used Google Dialogflow to handle client queries producing a 80% decrease in CSR workload <br />
-          • Led a team of frontend developers to architect a robust codebase using React + Redux <br />
-          • Maintained thorough documentation and unit tests with Jest/Enzyme
-          `,
-          tools: ['React.js', 'React Native', 'Redux', 'Jest/Enzyme', 'Node.js', 'Express.js', 'Firebase', 'Dialogflow']
-        },
-        {
-          src: require("./assets/q4.svg"),
-          srcWidth: 100,
-          srcHeight: 100,
-          title: "Q4 Inc.",
-          description: "QA Automation Developer",
-          body: `
-          • Responsible as the primary QA engineer for testing all web SaaS products working with tools such as
-          Postman, Selenium, and SQL <br />
-          • Automated regression testing for web products using Selenium, Java, and TestNG <br />
-          • Created automated test suites for the product’s front end and REST APIs increasing the testing 
-          workflow effeciency by more than 90% <br />
-          • Worked in a fast-paced, Agile environment as an active member of a Scrum team
-          `,
-          tools:['Java', 'Selenium', 'TestNG']
-
-        },
-        {
-          src: require("./assets/mockup1.png"),
-          srcWidth: 170,
-          srcHeight: 100,
-          title: "LooChat",
-          description: "Messaging/Video Chat app",
-          body: `
-          <a 
-            href="https://loochat.herokuapp.com" 
-            rel="noopener noreferrer"
-            target="_blank"
-            id="git-link"
-          >
-            https://loochat.herokuapp.com
-          </a> <br />
-          • Developed a P2P chat application that handles messages and video calls using Socket.io and WebRTC <br />
-          • Built web and mobile clients in React/React Native + Redux, paired with a Node/Express RESTful API <br />
-          • Implemented JWT token authentication
-          <br />
-          <a 
-            href="https://github.com/brnjlee/LooChat" 
-            rel="noopener noreferrer" 
-            target="_blank"
-            id="git-link"
-          >
-            Source Code
-          </a>
-          `,
-          tools: ['React.js', 'React Native', 'Redux', 'Node.js', 'Express.js', 'MongoDB', 'Socket.io', 'Simple-Peer(WebRTC)']
-        },
-        {
-          src: require("./assets/home.png"),
-          srcWidth: 100,
-          srcHeight: 100,
-          title: "RentForYou",
-          description: "Classified Ads for sublets",
-          body: `
-          • Created a classified ad platform where students can find housing options around their school area <br />
-          • Integrated Google Maps API to generate geocoded addresses and display rents onto a dynamic map
-          <br />
-          <a 
-            href="https://github.com/brnjlee/RentForYou" 
-            rel="noopener noreferrer" 
-            target="_blank"
-            id="git-link"
-          >
-            Source Code
-          </a>
-          `,
-          tools: ['Flask', 'MySQL', 'HTML', 'CSS', 'Javascript', 'Google Maps API']
-        }
-      ];
     this.headerTween = null;
     this.bodyTweenIn = null;
     this.bodyTweenOut = null;
@@ -111,85 +25,103 @@ class App extends Component {
     this.myTween = null;
     this.cards = [];
     this.links = [];
-    this.page2 = null
+    this.page2 = null;
   }
 
-  authenticate(){
-    return new Promise(resolve => setTimeout(resolve, 2000))
+  authenticate() {
+    return new Promise(resolve => setTimeout(resolve, 2000));
   }
 
   componentDidMount() {
-      this.authenticate().then(() => {
-        const ele = document.getElementById('loading-screen')
-        if(ele){
-          ele.classList.add('available')
+    this.authenticate().then(() => {
+      const ele = document.getElementById("loading-screen");
+      if (ele) {
+        ele.classList.add("available");
 
-          setTimeout(() => {
-            // remove from DOM
-            ele.outerHTML = '';
-          }, 2000)
-        }
-      })
+        setTimeout(() => {
+          // remove from DOM
+          ele.outerHTML = "";
+        }, 2000);
+      }
+    });
   }
 
-  openCard = (index) => {
+  openCard = index => {
     if (this.state.showCard === null) {
       this.setState({
         showCard: index
-      })
+      });
     }
-  }
+  };
 
-  closeCard = (index) => {
+  closeCard = index => {
     this.setState({
       showCard: null
-    })
-  }
-
+    });
+  };
 
   render() {
     if (this.state.pageTo === 1) {
       this.bodyTweenOut.restart();
       // this.headerTween.restart();
-    } else if(this.state.pageTo === 2) {
+    } else if (this.state.pageTo === 2) {
       this.bodyTweenIn.restart();
     }
 
     return (
-      <div id="master-wrapper" >
+      <div id="master-wrapper">
         <Scene isMobile={this.isMobile} />
-        <div id="header-main" ref={(i) => {this.headerMain = i}}>
-          <span id={this.isMobile? "name-mobile":"name"}>Brian Lee</span>
+        <div
+          id="header-main"
+          ref={i => {
+            this.headerMain = i;
+          }}
+        >
+          <span id={this.isMobile ? "name-mobile" : "name"}>Brian Lee</span>
         </div>
-        <div id="header-secondary" ref={(i) => {this.headerSecondary = i}}>
-          
-        </div>
+        <div
+          id="header-secondary"
+          ref={i => {
+            this.headerSecondary = i;
+          }}
+        ></div>
         {/* {this.state.page===1 && !this.isMobile && <div className="mouse"><span></span></div>} */}
-        <div id={this.isMobile? "mobile-icon-container":"icon-container"}>
-          <a 
-            href={resume} 
-            target="_blank" 
-            rel="noopener noreferrer"
-          >
-            <div className="icon" ref={(ref) => {this.links.push(ref)}}>
+        <div id={this.isMobile ? "mobile-icon-container" : "icon-container"}>
+          <a href={resume} target="_blank" rel="noopener noreferrer">
+            <div
+              className="icon"
+              ref={ref => {
+                this.links.push(ref);
+              }}
+            >
               <FiFileText />
             </div>
           </a>
-          <a 
-            href="https://github.com/brnjlee" 
-            rel="noopener noreferrer" 
+          <a
+            href="https://github.com/brnjlee"
+            rel="noopener noreferrer"
             target="_blank"
           >
-            <div className="icon" ref={(ref) => {this.links.push(ref)}}>
+            <div
+              className="icon"
+              ref={ref => {
+                this.links.push(ref);
+              }}
+            >
               <FiGithub />
             </div>
           </a>
-          <a 
-            href="https://www.linkedin.com/in/brian-lee-2553a1149/" 
-            rel="noopener noreferrer" 
+          <a
+            href="https://www.linkedin.com/in/brian-lee-2553a1149/"
+            rel="noopener noreferrer"
             target="_blank"
           >
-            <div className="icon" ref={(ref) => {this.links.push(ref)}}>
+            <div
+              className="icon"
+              ref={ref => {
+                this.links.push(ref);
+              }}
+            >
               <FiLinkedin />
             </div>
           </a>
--- src/components/Scene.js
+++ src/components/Scene.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import React3 from 'react-three-renderer';
-import * as THREE from 'three';
+import React3 from "react-three-renderer";
+import * as THREE from "three";
 
 class Scene extends Component {
   constructor(props, context) {
     super(props, context);
 
-    this.cameraPosition = new THREE.Vector3(0,0,50);
+    this.cameraPosition = new THREE.Vector3(0, 0, 50);
     this.particleCount = 100;
     this.particles = [];
     this.particleRefs = [];
@@ -21,67 +21,79 @@ class Scene extends Component {
 
     this._onAnimate = () => {
       if (this.props.isMobile) {
-        this.camera.position.x = (this.gyroY);
-        this.camera.position.y = -(this.gyroX);
+        this.camera.position.x = this.gyroY;
+        this.camera.position.y = -this.gyroX;
       } else {
-        this.camera.position.x -= (this.mouseX/30 + this.camera.position.x+10) * 0.05;
-        this.camera.position.y -= -(this.mouseY/30 - this.camera.position.y) * 0.05;
+        this.camera.position.x -=
+          (this.mouseX / 30 + this.camera.position.x + 10) * 0.05;
+        this.camera.position.y -=
+          -(this.mouseY / 30 - this.camera.position.y) * 0.05;
       }
       this.camera.lookAt(this.scene.position);
 
       for (let i = 0; i < this.particleCount; i++) {
-        if(this.particleRefs[i].direction.y > 0 && i<this.particleCount/2) {
+        if (
+          this.particleRefs[i].direction.y > 0 &&
+          i < this.particleCount / 2
+        ) {
           this.particleRefs[i].position.y += 0.1;
         } else {
           this.particleRefs[i].position.y -= 0.1;
         }
 
-        if(this.particleRefs[i].direction.x > 0 && i<this.particleCount/2) {
+        if (
+          this.particleRefs[i].direction.x > 0 &&
+          i < this.particleCount / 2
+        ) {
           this.particleRefs[i].position.x += 0.1;
         } else {
           this.particleRefs[i].position.x -= 0.1;
         }
 
-        if(this.particleRefs[i].position.x < -window.innerWidth || 
-          this.particleRefs[i].position.x > window.innerWidth ) {
+        if (
+          this.particleRefs[i].position.x < -window.innerWidth ||
+          this.particleRefs[i].position.x > window.innerWidth
+        ) {
           this.particleRefs[i].direction.x = -this.particleRefs[i].direction.x;
         }
-        if(this.particleRefs[i].position.y < -window.innerHeight || 
-          this.particleRefs[i].position.y > window.innerHeight ) {
+        if (
+          this.particleRefs[i].position.y < -window.innerHeight ||
+          this.particleRefs[i].position.y > window.innerHeight
+        ) {
           this.particleRefs[i].direction.y = -this.particleRefs[i].direction.y;
         }
-
       }
-    }
-    let particleGeometry = <sphereGeometry radius={20} widthSegments={20} heightSegments={20}/>;
-    let particleColors = [
-      0xf05f40,
-      0x405FF0,
-      0x5F40F0,
-      0xffff00,
-      0x33cc33
-    ];
+    };
+    let particleGeometry = (
+      <sphereGeometry radius={20} widthSegments={20} heightSegments={20} />
+    );
+    let particleColors = [0xffffff];
 
     for (let i = 0; i < this.particleCount; i++) {
       this.particles[i] = (
-        <mesh 
+        <mesh
           key={i}
-          ref={(particle) => {this.particleRefs.push(particle);
+          ref={particle => {
+            this.particleRefs.push(particle);
             this.particleRefs[i].direction = {
-              x:1,
-              y:1
-            }
+              x: 1,
+              y: 1
+            };
           }}
           position={
-            new THREE.Vector3( 
-              Math.random() * window.innerWidth * 2 - window.innerWidth, 
+            new THREE.Vector3(
+              Math.random() * window.innerWidth * 2 - window.innerWidth,
               Math.random() * window.innerHeight * 2 - window.innerHeight,
               -Math.random() * 700
             )
           }
         >
-         {particleGeometry}
-         <meshBasicMaterial color={particleColors[Math.floor(Math.random() * 5)]} opacity={0.5} transparent={true} />
+          {particleGeometry}
+          <meshBasicMaterial
+            color={particleColors[Math.floor(Math.random() * 5)]}
+            opacity={0.2}
+            transparent={true}
+          />
         </mesh>
       );
     }
@@ -89,52 +101,62 @@ class Scene extends Component {
 
   componentDidMount() {
     // window.addEventListener('wheel', this.handleScroll, false);
-    window.addEventListener('resize', this.onWindowResize, false);
-    document.addEventListener('mousemove', this.handleMouseMove, false);
+    window.addEventListener("resize", this.onWindowResize, false);
+    document.addEventListener("mousemove", this.handleMouseMove, false);
     if (this.props.isMobile) {
-      window.addEventListener('deviceorientation', this.handleOrientation);
+      window.addEventListener("deviceorientation", this.handleOrientation);
     }
   }
 
   componentWillUnmount() {
     // window.removeEventListener('wheel', this.handleScroll);
-    window.removeEventListener('resize', this.onWindowResize);
-    document.removeEventListener('mousemove', this.handleMouseMove);
+    window.removeEventListener("resize", this.onWindowResize);
+    document.removeEventListener("mousemove", this.handleMouseMove);
   }
 
-  handleOrientation = (e) => {
+  handleOrientation = e => {
     this.gyroX = e.beta;
     this.gyroY = e.gamma;
-  }
+  };
 
   onWindowResize = () => {
-    this.setState({
-      update: this.state.update + 1
-    });
-  }
+    // this.setState({
+    //   update: this.state.update + 1
+    // });
+  };
 
-  handleMouseMove = (e) => {
-        this.mouseX = e.clientX - window.innerWidth/2;
-        this.mouseY = e.clientY - window.innerHeight/2;
-  }
+  handleMouseMove = e => {
+    this.mouseX = e.clientX - window.innerWidth / 2;
+    this.mouseY = e.clientY - window.innerHeight / 2;
+  };
 
-  handleScroll = (e) => {
+  handleScroll = e => {
     e.preventDefault();
 
-    this.camera.rotation.set(this.camera.rotation.x + (e.deltaY * 0.005), this.camera.rotation.y+ (e.deltaY * 0.005), this.camera.rotation.z);
-    this.camera.position.set(this.camera.position.x , this.camera.position.y, this.camera.position.z+ (e.deltaY * 0.005))
-  
-    this.camera.position.clampScalar( 0, 10 );
-  }
+    this.camera.rotation.set(
+      this.camera.rotation.x + e.deltaY * 0.005,
+      this.camera.rotation.y + e.deltaY * 0.005,
+      this.camera.rotation.z
+    );
+    this.camera.position.set(
+      this.camera.position.x,
+      this.camera.position.y,
+      this.camera.position.z + e.deltaY * 0.005
+    );
+
+    this.camera.position.clampScalar(0, 10);
+  };
 
   render() {
     const width = window.innerWidth;
     const height = window.innerHeight;
-  
+
     return (
       <div id="canvas">
         <React3
-          ref={(renderer) => {this.renderer = renderer}}
+          ref={renderer => {
+            this.renderer = renderer;
+          }}
           mainCamera="camera"
           width={width}
           height={height}
@@ -143,20 +165,24 @@ class Scene extends Component {
           clearAlpha={1}
           onAnimate={this._onAnimate}
         >
-          <scene ref={(scene) => {this.scene = scene}}>
+          <scene
+            ref={scene => {
+              this.scene = scene;
+            }}
+          >
             <perspectiveCamera
-              ref={(camera) => {this.camera = camera}}
+              ref={camera => {
+                this.camera = camera;
+              }}
               name="camera"
               fov={75}
-              aspect={width/height}
+              aspect={width / height}
               near={0.1}
               far={1000}
-
               position={this.cameraPosition}
             />
             {this.particles}
-            <ambientLight intensity={.8} />
-            
+            <ambientLight intensity={0.8} />
           </scene>
         </React3>
       </div>
